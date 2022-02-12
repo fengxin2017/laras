6c721272275f7c24e727336e9338171909a9ee8d
fengxin <2169046620@qq.com>
2022-02-13 02:42:25 +0800

    update

--- storage/framework/views/2a4e9de21b0caf8db35341c81fca26bb9481cca0.php
+++
@@ -1,2 +0,0 @@
-[<?php echo e($slot); ?>](<?php echo e($url); ?>)
-<?php /**PATH /home/vagrant/code/maker/vendor/illuminate/mail/resources/views/text/header.blade.php ENDPATH**/ ?>
--- storage/framework/views/f201140058e953d39711b439d0ca1a45041a5ec4.php
+++
@@ -1,3 +0,0 @@
-<?php echo e($slot); ?>: <?php echo e($url); ?>
-
-<?php /**PATH /home/vagrant/code/maker/vendor/illuminate/mail/resources/views/text/button.blade.php ENDPATH**/ ?>
