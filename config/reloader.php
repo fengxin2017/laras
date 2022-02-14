7dbb4e3bc1c171ee0df9555719095b9fc7fc719d
冯欣冯欣2017 <>
2022-02-14 18:36:37 +0800

    update

--- config/reloader.php
+++ config/reloader.php
@@ -2,13 +2,15 @@
 
 return [
     'on' => true,
-    // 某些修改（例如修改切面文件的注解）热载是不起作用的，请自动重启服务重新生产代理文件
+    // 注解和切面的收集发生在server进程启动前，故（例如修改切面文件的注解的操作）AnnotationCollector
+    //和AspectCollector中的container不会发生变化，此时请自行重启服务。
     'driver' => 'hash',
     'hash' => [
         'watch_path' => [
             ROOT_PATH . DIRECTORY_SEPARATOR . 'app',
         ]
     ],
+    // 不建议用此方式
     // inotify only support edit in virtual env. edit code with phpstorm use hash driver plaz.
     'inotify' => [
         'watch_path' => [
